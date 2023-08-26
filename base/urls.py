3ff1cb2a2e7f3bb6feaec50e4b84f61266a235ab
Antonio-Acosta <antonio.acosta@ibm.com>
2023-08-26 00:43:58 -0600

    Register Transaction model in admin.py

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('', views.endpoints),
-    path('users/', views.user_list),
+    path('users/', views.UserList.as_view()),
     path('users/<str:username>', views.UserDetail.as_view()),
     path('users/<str:username>/accounts/', views.account_list),
     path('users/<str:username>/accounts/<str:account_name>', views.AccountDetail.as_view()),
